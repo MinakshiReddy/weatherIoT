be936b973d5ed92e4199474f0886cdb93708ef23
MinakshiReddy <studentminakshee@gmail.com>
2020-12-10 18:24:17 +0530

    upload

---
+++ src/main/java/com/ssl/weather/Homepage.java
@@ -0,0 +1,19 @@
+package com.ssl.weather;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Homepage {
+
+	@GetMapping("/homepage")
+	public String homepage(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "homepage";
+	}
+	
+
+
+}
